b037d619bf4c79d61497a78c304b429aff7367b5
Dat on da PC <tranhungdat.aus@gmail.com>
2025-02-08 10:57:30 +1100

    commit

---
+++ data gathering/addy.py
@@ -0,0 +1,12 @@
+npc = "https://stardewvalleywiki.com/Villagers"
+
+# Goal: Build a dataset of characters, how they say it, when they say it, and the face they make
+# 1: Scrape the description of each NPC
+# 2: Scrape the text of each NPC's heart events
+# 3: Build a json dataset
+
+# Further goals:
+'''
+- Scrape the schedule, likes and dislikes
+- c
+'''

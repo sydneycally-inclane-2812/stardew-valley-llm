18311d9d727f44a415680d593515178faaaca8b9
Dat on da PC <tranhungdat.aus@gmail.com>
2025-02-09 14:45:16 +1100

    uhh what

--- data gathering/addy.py
+++ data gathering/addy.py
@@ -1,4 +1,9 @@
 npc = "https://stardewvalleywiki.com/Villagers"
+npc_pages = ['/Alex','/Elliott','/Harvey','/Sam','/Sebastian','/Shane','/Abigail','/Emily','/Haley','/Leah',
+             '/Maru', '/Penny','/Caroline', '/Clint', '/Demetrius', '/Dwarf', '/Evelyn', '/George', '/Gus',
+             '/Jas', '/Jodi', '/Kent', '/Krobus', '/Leo', '/Lewis', '/Linus', '/Marnie', '/Pam', '/Pierre',
+             '/Robin', '/Sandy', '/Vincent', '/Willy', '/Wizard', '/Birdie', '/Bouncer', '/Fizz', '/Gil',
+              '/Governor', '/Grandpa', '/Gunther', '/Henchman', '/Marlon', '/Morris', '/Mr._Qi', '/Professor_Snail']
 
 # Goal: Build a dataset of characters, how they say it, when they say it, and the face they make
 # 1: Scrape the description of each NPC
